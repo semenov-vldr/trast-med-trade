825f31572e0cabd867ddea7a267984bf2dc6107b
semenov-vldr <semenov-vldr@users.noreply.github.com>
2025-05-08 07:36:24 +0000

    deploy: 4efe05c170fb45d685c06dbd0cfb3f05a4690dcf

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -258,6 +258,45 @@ if (partners) {
 }
 "use strict";
 
+// Слайдер с миниатюрами
+var productPage = document.querySelector(".product-page");
+if (productPage) {
+  createProductProfileSlider(productPage);
+}
+function createProductProfileSlider(productPage) {
+  var swiperMain = productPage.querySelector('.swiper-main');
+  var swiperThumbs = productPage.querySelector('.swiper-thumbs');
+  var swiper__thumbs = new Swiper(swiperThumbs, {
+    spaceBetween: 28,
+    slidesPerView: "auto",
+    freeMode: true,
+    watchSlidesProgress: true,
+    watchSlidesVisibility: true,
+    watchOverflow: true,
+    initialSlide: 0,
+    direction: "vertical"
+  });
+  var swiper__main = new Swiper(swiperMain, {
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+    loop: true,
+    slidesPerView: 1,
+    centeredSlides: true,
+    initialSlide: 0,
+    thumbs: {
+      swiper: swiper__thumbs
+    },
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true
+    }
+  });
+}
+;
+"use strict";
+
 var products = document.querySelector(".products");
 if (products) {
   var productSwiper = products.querySelector(".products__slider");
