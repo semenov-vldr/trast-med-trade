71a4418540989b901ed912ef198ce8d6410cd6e0
Vladimir <vova2066@yandex.ru>
2025-05-12 22:43:27 +0300

    ref js of details

--- src/components/details-doc/details-doc.js
+++ src/components/details-doc/details-doc.js
@@ -1,7 +1,9 @@
 // Раскрытие только одного "details" в блоке "Документация"
-const docDetails = productPage.querySelectorAll(".product-page__details-doc .product-page__details-doc-item");
-docDetails.forEach(detailsItem => {
-  detailsItem.addEventListener("click", function () {
-    docDetails.forEach(d => d !== this ? d.removeAttribute("open") : "null");
+const docDetails = document.querySelectorAll(".details-doc");
+if (docDetails) {
+  docDetails.forEach(detailsItem => {
+    detailsItem.addEventListener("click", function () {
+      docDetails.forEach(d => d !== this ? d.removeAttribute("open") : "null");
+    });
   });
-});
+}
